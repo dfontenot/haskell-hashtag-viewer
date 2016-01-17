42aaf13ee3f6c35c7c1113e0690225004ce5c19b
David Fontenot <davidmfontenot@gmail.com>
2016-01-17 15:56:51 -0800

    Starting over, current impl works, TODO warnings
    Use OAuth and HTTP directly. TODO is to clean
    up two warnings.
    When ran, finds recent tweets with #cats in the tweet,
    dumps the raw JSON response

---
+++ Readme.md
@@ -0,0 +1,8 @@
+A Twitter slideshow server in Haskell
+=================
+
+Poll for a given hashtag and store any photos found in the tweets
+
+Keys / secrets
+--------------
+Store in private/
---
+++ src/Main.hs
@@ -0,0 +1,46 @@
+{-#LANGUAGE OverloadedStrings #-}
+
+module Main where
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BC
+import Data.Char (isSpace)
+import Web.Authenticate.OAuth
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Control.Monad
+
+-- https://dev.twitter.com/rest/public/search
+searchQuery :: String
+searchQuery = "https://api.twitter.com/1.1/search/tweets.json?q=%23cats"
+
+myOAuth :: B.ByteString -> B.ByteString -> OAuth
+myOAuth consumerKey consumerSecret =
+  newOAuth { oauthConsumerKey = consumerKey,
+             oauthConsumerSecret = consumerSecret }
+
+getResponse :: Request -> IO BL.ByteString
+getResponse request = withManager tlsManagerSettings
+  (\manager ->
+    (liftM responseBody) $ httpLbs request manager)
+
+readFileStripWhitespace :: String -> IO B.ByteString
+readFileStripWhitespace fn = liftM BC.pack filteredContents
+  where
+    filteredContents = liftM (filter (not . isSpace)) $ readFile fn
+
+main :: IO ()
+main = do
+  consumerKey <- readFileStripWhitespace "private/consumer.key"
+  consumerSecret <- readFileStripWhitespace "private/consumer.secret"
+  accessToken <- readFileStripWhitespace "private/access.token"
+  accessSecret <- readFileStripWhitespace "private/access.secret"
+
+  myRequest <- parseUrl searchQuery
+
+  let oauthConsumer = myOAuth consumerKey consumerSecret in
+    let creds = newCredential accessToken accessSecret in
+      do
+        signedRequest <- signOAuth oauthConsumer creds myRequest
+        response <- getResponse signedRequest
+        BL.putStrLn $ response
