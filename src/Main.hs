7623ab887b3643d1a392fc6a655ba456e4c29171
David Fontenot <davidmfontenot@gmail.com>
2016-01-30 18:09:14 -0800

    integrate w/ sqlite, can store tweets in sqlite

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,24 @@
 {-#LANGUAGE OverloadedStrings #-}
 
 module Main where
-import TwitterTypes
+import qualified TwitterTypes as TT
+import SqlTypes
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as BC
-import qualified Data.Text.IO as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+import qualified Database.SQLite.Simple as DB
 import Data.Aeson
 import Data.Char (isSpace)
 import Web.Authenticate.OAuth
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS (tlsManagerSettings)
 import Control.Monad
+
+dbFile :: String
+dbFile = "tweets.db"
 
 -- https://dev.twitter.com/rest/public/search
 searchQuery :: String
@@ -33,8 +39,8 @@ readFileStripWhitespace fn = liftM BC.pack filteredContents
   where
     filteredContents = liftM (filter (not . isSpace)) $ readFile fn
 
-main :: IO ()
-main = do
+getLatestTweets :: IO (Either String TT.TwitterSearch)
+getLatestTweets = do
   consumerKey <- readFileStripWhitespace "private/consumer.key"
   consumerSecret <- readFileStripWhitespace "private/consumer.secret"
   accessToken <- readFileStripWhitespace "private/access.token"
@@ -47,7 +53,29 @@ main = do
       do
         signedRequest <- signOAuth oauthConsumer creds myRequest
         response <- getResponse signedRequest
-        let jsonTweets = (eitherDecode response) :: (Either String TwitterSearch) in
-          case jsonTweets of
-            Left err -> putStrLn err
-            Right search -> T.putStrLn $ text (head (statuses search))
+        return ((eitherDecode response) :: (Either String TT.TwitterSearch))
+
+statusToTweetFields :: TT.Status -> TweetFields
+statusToTweetFields status = (TweetFields (TT.id status) (TT.screen_name (TT.user status)) (TT.text status))
+
+createTableIfNotExists :: IO ()
+createTableIfNotExists = DB.withConnection dbFile
+  (\conn ->
+    DB.execute_ conn "CREATE TABLE IF NOT EXISTS tweets (id INTEGER PRIMARY KEY, tweet_id INT, author VARCHAR(50), text VARCHAR(140))")
+
+writeTweetsToDB :: [TT.Status] -> IO ()
+writeTweetsToDB tweets =
+  DB.withConnection dbFile (executeStatements tweets)
+  where
+    executeStatements [] _ = return ()
+    executeStatements (status:rst) conn = do
+      DB.execute conn "INSERT INTO tweets (tweet_id,author,text) values (?,?,?)" (statusToTweetFields status)
+      executeStatements rst conn
+
+main :: IO ()
+main = do
+  createTableIfNotExists
+  tweetResult <- getLatestTweets
+  case tweetResult of
+    Left err -> putStrLn err
+    Right tweets -> writeTweetsToDB $ TT.statuses tweets
--- src/TwitterTypes.hs
+++ src/TwitterTypes.hs
@@ -5,8 +5,17 @@ import Data.Aeson
 import Data.Text
 import GHC.Generics
 
+data User =
+  User { name :: Text
+       , screen_name :: Text } deriving (Show, Generic)
+
+instance FromJSON User
+instance ToJSON User
+
 data Status =
   Status { text :: Text
+         , id :: Integer
+         , user :: User
          , created_at :: Text } deriving (Show, Generic)
 
 instance FromJSON Status
