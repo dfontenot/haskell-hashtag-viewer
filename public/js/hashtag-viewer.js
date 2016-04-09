feda080112e09bf9904cff80318871ef96028ff7
David Fontenot <davidmfontenot@gmail.com>
2016-04-08 23:26:11 -0700

    load a new random image every 2 seconds

--- public/js/hashtag-viewer.js
+++ public/js/hashtag-viewer.js
@@ -1,13 +1,40 @@
-function onGetSuccess(data) {
+var displayTimeMs = 2000; // how long to show each image
+var curData = null;
+
+var renderImage = function(data) {
   $('body').html("<img src='data:" + data.mimeType + ";base64," + data.image + "' />");
-}
+};
+
+var renderNoImagesYet = function() {
+  $('body').html("<p>No images yet</p>");
+};
+
+var showImageAndGetNext = function() {
+
+  if (curData == null) {
+    renderNoImagesYet();
+  }
+  else {
+    renderImage(curData);
+  }
+
+  // get next image in background
+  $.ajax({
+    url: 'http://localhost:8000',
+    type: 'GET',
+    crossDomain: true,
+    success: function(data) {
+      curData = data;
+    },
+    error: function() {
+      console.log("Issue loading the next image");
+    }
+  });
+
+  // start timer again
+  setTimeout(showImageAndGetNext, displayTimeMs);
+};
 
 $(document).ready(function() {
-  var request = $.ajax({
-    url: "http://localhost:8000",
-    type: "GET",
-    crossDomain: true,
-    success: onGetSuccess,
-    error: function() { console.log("failure") }
-  });
+  setTimeout(showImageAndGetNext, displayTimeMs);
 });
