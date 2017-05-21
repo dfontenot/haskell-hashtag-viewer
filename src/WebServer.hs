947d6c7eec9e9f41cb143307a2041a072c51438d
David Fontenot <davidmfontenot@gmail.com>
2017-05-21 11:47:42 -0700

    fixed TODO where redirect always uses port 8001
    passed reader context through another function layer
    so that the redirection headers match the port the
    user specified
    
    code is functional, but lots of unnecessary do blocks remain

--- src/WebServer.hs
+++ src/WebServer.hs
@@ -6,11 +6,12 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Map.Strict as Map
 import qualified System.FilePath as FP
+import qualified Network.URL as URL
 import Data.String.Conversions
+import Data.Maybe
 import Data.Word
 import Network.HTTP.Server
 import Network.HTTP.Server.Logger
-import Network.URL
 import Network.Mime
 import Control.Monad
 import Control.Monad.Reader
@@ -20,7 +21,7 @@ import System.Environment
 
 import SimpleArgvParser
 
-data ServerSettings = ServerSettings { webRoot :: String }
+data ServerSettings = ServerSettings { webRoot :: String, port :: Int }
 
 data FileResponse =
   PermissionDenied String
@@ -35,18 +36,20 @@ headers = [Header HdrPragma "no-cache"
           , Header HdrConnection "close"
           , Header HdrServer "HashtagViewerWebServer"]
 
-redirectHeaders :: [Header]
-redirectHeaders = [Header HdrPragma "no-cache"
-                  , Header HdrConnection "keep-alive"
-                  , Header HdrLocation "http://localhost:8001/index.html"
-                  , Header HdrServer "HashtagViewerWebServer"]
+redirectHeaders :: Int -> [Header]
+redirectHeaders port = [Header HdrPragma "no-cache"
+                       , Header HdrConnection "keep-alive"
+                       , Header HdrLocation $ "http://localhost:" ++ (show port) ++ "/index.html"
+                       , Header HdrServer "HashtagViewerWebServer"]
 
-respondForFile :: FileResponse -> IO (Response BL.ByteString)
+respondForFile :: FileResponse -> ServerMonad (Response BL.ByteString)
 respondForFile (PermissionDenied path) = return $ Response (4,0,3) "Permission denied" headers $ BL.append "Permission denied: " (cs path)
 respondForFile (FileNotFound path) = return $ Response (4,0,4) "File not found" headers $ BL.append "File not found: " (cs path)
-respondForFile RedirectToIndex = return $ Response (3,0,1) "Moved Permanently" redirectHeaders "Redirect"
+respondForFile RedirectToIndex = do
+  port <- asks port
+  return $ Response (3,0,1) "Moved Permanently" (redirectHeaders port) "Redirect"
 respondForFile (FileOK path) = do
-  contents <- BL.readFile path
+  contents <- liftIO $ BL.readFile path
   return $ Response (2,0,0) "Ok" modifiedHeaders contents
     where
       mimeTypeHeader = Header HdrContentType $ cs $ defaultMimeLookup (cs path)
@@ -67,25 +70,26 @@ getResponseForFile urlPath = do
                             else return $ PermissionDenied filePath)
          else return $ FileNotFound filePath
 
-doRespond :: URL -> ServerMonad (Response BL.ByteString)
+doRespond :: URL.URL -> ServerMonad (Response BL.ByteString)
 doRespond url = do
-  file <- getResponseForFile $ url_path url
-  liftIO $ respondForFile file
+  file <- getResponseForFile $ URL.url_path url
+  respondForFile file
 
-handler :: URL -> Request BL.ByteString -> ServerMonad (Response BL.ByteString)
+handler :: URL.URL -> Request BL.ByteString -> ServerMonad (Response BL.ByteString)
 handler url req = case rqMethod req of
   GET -> doRespond url
   _ -> return $ err_response NotImplemented
 
+getPortFromArgs :: (Num a) => Map.Map String String -> a
+getPortFromArgs argMap = case Map.lookup "port" argMap of
+                           Just port -> fromIntegral (read port :: Integer)
+                           Nothing -> 8001
+
+getBindAddressFromArgs :: Map.Map String String -> String
+getBindAddressFromArgs argMap = fromMaybe "localhost" $ Map.lookup "bind" argMap
+
 makeServerConfig :: Map.Map String String -> Config
-makeServerConfig argMap = Config stdLogger bindAddress port
-  where
-    bindAddress = case Map.lookup "bind" argMap of
-      Just address -> address
-      Nothing -> "localhost"
-    port = case Map.lookup "port" argMap of
-      Just port -> fromIntegral (read port :: Integer)
-      Nothing -> 8001
+makeServerConfig argMap = Config stdLogger (getBindAddressFromArgs argMap) (getPortFromArgs argMap)
 
 main :: IO ()
 main = do
@@ -94,9 +98,9 @@ main = do
   -- TODO: could be made cleaner
   case pairArguments args of
     Just argMap -> case Map.lookup "root" argMap of
-      Just web_root -> serverWith (makeServerConfig argMap) $ handler' web_root
+      Just web_root -> serverWith (makeServerConfig argMap) $ handler' web_root (getPortFromArgs argMap)
       Nothing -> putStrLn usage
     Nothing -> putStrLn usage
   where
-    handler' webRoot _ url req = runReaderT (handler url req) (ServerSettings webRoot)
+    handler' webRoot port _ url req = runReaderT (handler url req) (ServerSettings webRoot port)
     usage = "./WebServer --root <web_root> [--port <port>] [--bind <address>]"
